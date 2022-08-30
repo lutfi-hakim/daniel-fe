ada06ea63f705f5d32d8cc5455ac4eb69cabb5d5
Ahmadkun88 <mahyailahmad@gmail.com>
2022-08-30 19:34:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
                   height="20"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   className="mx-3"
                   role="img"
                   viewBox="0 0 24 24"
@@ -79,62 +79,53 @@ function App() {
       <section>
         <Container>
           <div className="hero-daniel">
-            <div className=" mb-4 text-white rounded row">
-              <div className="col-md-6 px-0 text-hero">
-                <h1 className="display-4">
-                  Timeless insights for business and life.
-                </h1>
-                <p className="lead my-3">
-                  Every week, I sit down with someone in the Top 1% of their
-                  field — from NYT bestselling authors to Oscar winners — to
-                  decode what they've mastered. Listen as I uncover the tactics,
-                  routines, and habits that will propel you to the top of your
-                  field.
-                </p>
-                {/* <p className="lead mb-0">
+            <div className=" px-0 text-hero">
+              <h1 className="display-4">
+                Timeless insights for business and life.
+              </h1>
+              <p className="lead my-3">
+                Every week, I sit down with someone in the Top 1% of their field
+                — from NYT bestselling authors to Oscar winners — to decode what
+                they've mastered. Listen as I uncover the tactics, routines, and
+                habits that will propel you to the top of your field.
+              </p>
+              {/* <p className="lead mb-0">
                   <a href="#" className="text-white fw-bold">
                     Continue reading...
                   </a>
                 </p> */}
-                <div className="boton-hero">
-                  <div className="up">
-                    <div className="card-hero">
-                      <img
-                        src="https://assets-global.website-files.com/5f4f71d4aeab4787c7926d10/63050c840e48d9bc407fc55b_Andrew-Herr_Thumbnail.jpg"
-                        alt=""
-                      />
-                      <div className="title">
-                        <span>Lates Periode</span>
-                        <p>
-                          #128 Andrew Herr of Fount: My Favorite Books, Tools,
-                          Habits and More | 20 Minute Playbook
-                        </p>
-                      </div>
+              <div className="boton-hero">
+                <div className="up">
+                  <div className="card-hero">
+                    <img
+                      src="https://assets-global.website-files.com/5f4f71d4aeab4787c7926d10/63050c840e48d9bc407fc55b_Andrew-Herr_Thumbnail.jpg"
+                      alt=""
+                    />
+                    <div className="title">
+                      <span>Lates Periode</span>
+                      <p>
+                        #128 Andrew Herr of Fount: My Favorite Books, Tools,
+                        Habits and More | 20 Minute Playbook
+                      </p>
                     </div>
                   </div>
-                  <div className="ob">
-                    <a
-                      className="btn btn-sm btn-outline-secondary px-4"
-                      href="#"
-                    >
-                      Start Hero
-                    </a>
-                    <a
-                      className="btn btn-sm btn-outline-secondary px-4"
-                      href="#"
-                    >
-                      Listen to the Podcast
-                    </a>
-                  </div>
+                </div>
+                <div className="ob">
+                  <a className="btn btn-sm btn-outline-secondary px-4" href="#">
+                    Start Hero
+                  </a>
+                  <a className="btn btn-sm btn-outline-secondary px-4" href="#">
+                    Listen to the Podcast
+                  </a>
                 </div>
               </div>
-              <div className="col-md-6 px-0">
-                <img
-                  src="https://assets-global.website-files.com/5f46baf9863467a01b1aaab9/5f479acca51c91168e689008_card-portrait-tiny.png"
-                  alt=""
-                  className="image-daniel"
-                />
-              </div>
+            </div>
+            <div className="px-0">
+              <img
+                src="https://assets-global.website-files.com/5f46baf9863467a01b1aaab9/5f479acca51c91168e689008_card-portrait-tiny.png"
+                alt=""
+                className="image-daniel"
+              />
             </div>
           </div>
         </Container>
@@ -168,14 +159,56 @@ function App() {
         </div>
       </section>
       <section>
-        <Container>
+        {/* <Container>
           <Episode />
+        </Container> */}
+      </section>
+      <section>
+        <Container>
+          <div className="sam">
+            <div className="card-sam">
+              <img
+                src="https://assets-global.website-files.com/5f46baf9863467a01b1aaab9/5f48e96eee96f56ecaa77e0b_popular-white.svg"
+                alt=""
+              />
+              <h4>TRENDING NOW</h4>
+              <p>Various versions have evolved over the year</p>
+            </div>
+
+            <div className="card-sam">
+              <img
+                src="https://assets-global.website-files.com/5f46baf9863467a01b1aaab9/5f54b4db31df4cefcc660906_latest-posts-white.svg"
+                alt=""
+              />
+              <h4>NEW RELEASES</h4>
+              <p>Various versions have evolved over the year</p>
+            </div>
+
+            <div className="card-sam">
+              <img
+                src="https://assets-global.website-files.com/5f46baf9863467a01b1aaab9/5f5776bf1f735a4480fcc81f_topics-and-tags-white.svg"
+                alt=""
+              />
+              <h4>THE ARCHIVES</h4>
+              <p>Various versions have evolved over the year</p>
+            </div>
+          </div>
         </Container>
       </section>
       <footer className="py-5">
         <Container>
-          <div className="row">
-            <div className="col-6 col-md-2 mb-3">
+          <div className="row cus-foot">
+            <div className="col-md-6 col-sm-12 mb-3">
+              <div className="abo">
+                <p>
+                  Daniel Scrivner is an entreprenuer and investor focused on
+                  building enduring companies and breakthrough products.
+                </p>
+                <a href="#">See what he’s working on now</a>
+                <h1>Daniel Scrivner</h1>
+              </div>
+            </div>
+            <div className="col-12 col-md-2 mb-3">
               <h5>Section</h5>
               <ul className="nav flex-column">
                 <li className="nav-item mb-2">
@@ -206,7 +239,7 @@ function App() {
               </ul>
             </div>
 
-            <div className="col-6 col-md-2 mb-3">
+            <div className="col-12 col-md-2 mb-3">
               <h5>Section</h5>
               <ul className="nav flex-column">
                 <li className="nav-item mb-2">
@@ -237,7 +270,7 @@ function App() {
               </ul>
             </div>
 
-            <div className="col-6 col-md-2 mb-3">
+            <div className="col-12 col-md-2 mb-3">
               <h5>Section</h5>
               <ul className="nav flex-column">
                 <li className="nav-item mb-2">
@@ -266,52 +299,25 @@ function App() {
                   </a>
                 </li>
               </ul>
-            </div>
-
-            <div className="col-md-5 offset-md-1 mb-3">
-              <form>
-                <h5>Subscribe to our newsletter</h5>
-                <p>Monthly digest of what's new and exciting from us.</p>
-                <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                  <label htmlFor="newsletter1" className="visually-hidden">
-                    Email address
-                  </label>
-                  <input
-                    id="newsletter1"
-                    type="text"
-                    className="form-control"
-                    placeholder="Email address"
-                  />
-                  <button className="btn btn-primary" type="button">
-                    Subscribe
-                  </button>
-                </div>
-              </form>
             </div>
           </div>
 
-          <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+          <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top text-muted">
             <p>&copy; 2022 Company, Inc. All rights reserved.</p>
-            <ul className="list-unstyled d-flex">
+            <ul className="list-unstyled d-flex ">
               <li className="ms-3">
-                <a className="link-dark" href="#">
-                  <svg className="bi" width="24" height="24">
-                    {/* <use xlink:href="#twitter" /> */}
-                  </svg>
+                <a className="nav-link link-dark text-muted" href="#">
+                  Privacy & Terms
                 </a>
               </li>
               <li className="ms-3">
-                <a className="link-dark" href="#">
-                  <svg className="bi" width="24" height="24">
-                    {/* <use xlink:href="#instagram" /> */}
-                  </svg>
+                <a className="nav-link link-dark text-muted" href="#">
+                  Disclosures
                 </a>
               </li>
               <li className="ms-3">
-                <a className="link-dark" href="#">
-                  <svg className="bi" width="24" height="24">
-                    {/* <use xlink:href="#facebook" /> */}
-                  </svg>
+                <a className="nav-link link-dark text-muted" href="#">
+                  Sitemap
                 </a>
               </li>
             </ul>
--- src/Episode.js
+++ src/Episode.js
@@ -1,59 +1,111 @@
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
+import Slider from "react-slick";
+
+const products = [
+  {
+    title:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+  },
+  {
+    title:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+];
 
 function Episode() {
+  const config = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <CardGroup>
-      <Card>
-        <Card.Img
-          variant="top"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjWh1yoRkNAbZuzMuQfcvZ2RgS3yVhyeLVfQ&usqp=CAU"
-        />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img
-          variant="top"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjWh1yoRkNAbZuzMuQfcvZ2RgS3yVhyeLVfQ&usqp=CAU"
-        />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{" "}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img
-          variant="top"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjWh1yoRkNAbZuzMuQfcvZ2RgS3yVhyeLVfQ&usqp=CAU"
-        />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
+    <>
+      <div className="sliderse">
+        <Slider {...config}>
+          {products.map((x, i) => {
+            return (
+              <div className="course_card" key={i}>
+                <div className="course_card_img">
+                  <img
+                    src="https://images.unsplash.com/photo-1521791055366-0d553872125f?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjExNzczfQ"
+                    alt="course"
+                  />
+                </div>
+                <div className="course_card_content">
+                  <h3 className="title">Angular Typescript</h3>
+                  <p className="description">
+                    When we are no longer able to change a situation - we are
+                    challenged to change ourselves. Spread love everywhere you
+                    go. Let no one ever come to you without leaving happier.
+                    Problems are not stop signs, they are guidelines.
+                  </p>
+                </div>
+                <div className="course_card_footer">
+                  <a
+                    className="nav-item"
+                    href="https://www.smartcodehub.com/blog"
+                  >
+                    Add Review
+                  </a>
+                  <a
+                    className="nav-item"
+                    href="https://www.smartcodehub.com/blog"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    </>
   );
 }
 
